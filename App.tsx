344500be4a0078bb5ac79e497035aa37ac32abe9
MarianaSavoldi <marianasavodi123@gmail.com>
2022-09-26 12:09:54 -0300

    main screen layout

--- App.tsx
+++ App.tsx
@@ -1,25 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+const imagePath = require('./assets/images/rpg-door.jpeg');
+
+const COLORS = {
+  TITLE: "#FFC700",
+  BACKGROUND: "#363636"
+}
 
 export default function App() {
+  let[fontsLoaded, error]=useFonts({
+    "The-Witcher-Font": require('./assets/fonts/Thewitcher-jnOj.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>todo mundo dando o rego</Text>
-      <Text style={styles.content}>Aiiii texto em vermelho</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={imagePath} resizeMode="cover" style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Text style={styles.title}>RPG Master</Text>
+        <Text style={styles.content}>Entrar</Text>
+        <Text style={styles.content}>Cadastrar</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title:{
+    color: COLORS.TITLE,
+    fontSize: 80,
+    fontFamily: "The-Witcher-Font",
+    jutifyContent: "flex-start",
+    marginTop: 70,
+    marginBottom: 310,
+    paddingBottom: 100,
+    textAlign: "center"
   },
   content: {
-    color: "red"
-  }
+    alignSelf: "center",
+    backgroundColor: COLORS.BACKGROUND,
+    borderRadius: 8,
+    color: COLORS.TITLE,
+    fontFamily: "The-Witcher-Font",
+    fontSize: 30,
+    justifyContent: "center",
+    margin: 10,
+    padding: 10,
+    paddingBottom: 7
+  },
+  backgroundImage: {
+    flex: 1
+  },
 });
