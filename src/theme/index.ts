4c7f1318131b86397e4c96ba903517c3bdcc3308
MarianaSavoldi <marianasavodi123@gmail.com>
2022-10-24 16:17:12 -0300

    Adding a theme and removing its path mapping

---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+export default {
+  COLORS: {
+    BLACK: '#000000',
+    DARK_GREY: '#363636',
+    GOLD: '#FFD700',
+    GREY: '#C4C4C4',
+    WHITE: '#FFFFFF',
+    YELLOW: '#FFC700',
+  },
+  FONT_FAMILY: {
+    TITLE: '@assets/fonts/Thewitcher-jnOj.ttf',
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24
+  }
+}
