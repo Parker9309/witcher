025abab6574d485110d814b5f0f26379659e68ae
MarianaSavoldi <marianasavodi123@gmail.com>
2022-10-24 17:20:54 -0300

    Add theme types

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,12 +2,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
-  align-items: center;
-  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.DARK_GREY};
+  padding: 24px;
 `;
 
-export const Title = styled.Text`
-  color: #fff;
-  font-size: 32px;
-`;
+// export const Title = styled.Text`
+//   color: #fff;
+//   font-size: 32px;
+// `;
